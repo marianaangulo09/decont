a77118f593d86540753f3fe035cc3ac118354b34
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-13 19:44:23 +0100

    Fix error 1
    In this version I have fixed the first error i got, which is that the script was not finding the urls file.

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,11 +3,14 @@
 #Assign the variables 
 
 # absolute path for the provided file
-url_file="$(realpath "$1")"
 destination_directory="/home/mariana/Linux_entregable/decont/data"
 uncompress=$3
 exclude_keyword=$4
 expected_md5=$5
+
+
+# Look for a file named 'urls' in the destination directory
+url_file="$destination_directory/urls"
 
 #Create a  the directory for the downloaded data, as for this exercise we already have the data directory i set the pathway in the variable 
 mkdir -p "$destination_directory"
