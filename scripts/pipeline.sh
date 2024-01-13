5370d7ea1f57fb17f76a9f5b563949c923655dc0
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-13 22:50:17 +0100

    This is the first version of the pipeline
    In this version the first to segments of the pipeline have been added: 1. Download files, 2:download contaminants datase

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,3 +1,25 @@
+
+#Download all the files specified in data/filenames
+#For download.sh you can add 4 arguments 1:path were the url file is, 2: the path of the directory to store the downloaded files, 3:option yes to uncompress, 4: specific word to filter
+
+# Specify the list of URLs
+list_of_urls="/home/mariana/Linux_entregable/decont/data/urls"
+
+# Download all the files specified in the list_of_urls
+for url in $(<"$list_of_urls"); do
+    bash download.sh "/home/mariana/Linux_entregable/decont/data/urls" "/home/mariana/Linux_entregable/decont/data"  yes "exclude_keyword" 
+done
+
+# Download the contaminants fasta file, uncompress it, and
+# filter to remove all small nuclear RNAs
+
+bash download.sh "/home/mariana/Linux_entregable/decont/data/cont" "/home/mariana/Linux_entregable/decont/res"  yes #TODO
+
+
+
+
+
+
 #Download all the files specified in data/filenames
 for url in $(<list_of_urls>) #TODO
 do
