361f338fdf189dcfb4c75c2dfe6435b0d5717654
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-14 00:31:19 +0100

    This version had an update of the variables incluiding the complete path so we dont have troubles in finding files
    IN this version there is an update of the arguments and now it asks for the complete pathway to avoid erros in indentifying the files, in adittion this version has the merge seccion

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,13 +1,17 @@
 
 # Assign the input arguments to variables
-samples_directory="$1"
-output_directory="$2"
-sample_id="$3"
+samples_directory="$(realpath "$1")"
+output_directory="$(realpath "$2")"
+sample_id="$(realpath "$3")"
 
 # Create the output directory if it doesn't exist
 mkdir -p "$output_directory"
 
 
+# Merge all files for the specified  sample id  into a single file
+cat "$samples_directory/$sample_id"*.fastq > "$output_directory/$sample_id_merged.fastq"
+
+echo "Merge completed. Merged file saved in '$output_directory'."
 
 
 
