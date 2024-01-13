666e06632c9ee2fd93014cdff1f8af633d93624c
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-14 00:26:26 +0100

    THis is the first version of the merge script
    Here the variable assign and the output directory is created

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,17 @@
+
+# Assign the input arguments to variables
+samples_directory="$1"
+output_directory="$2"
+sample_id="$3"
+
+# Create the output directory if it doesn't exist
+mkdir -p "$output_directory"
+
+
+
+
+
+
 # This script should merge all files from a given sample (the sample id is
 # provided in the third argument ($3)) into a single file, which should be
 # stored in the output directory specified by the second argument ($2).
