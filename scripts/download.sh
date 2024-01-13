ad22ae214b51fcdf5931f5070d81df8081feceac
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-13 22:44:41 +0100

    This version has been modified in order to make it suitable to work as part of the pipeline
    In this version the arguments for the url file and for the destination of the downloads have been leave to be generic so the code can be used for the genomes and also for the contamintans

--- scripts/download.sh
+++ scripts/download.sh
@@ -8,8 +8,6 @@ destination_directory="$(realpath "$2")"
 uncompress=$3
 exclude_keyword=$4
 
-# Look for a file named 'urls' in the destination directory
-url_file="$destination_directory/urls"
 
 #Create a  the directory for the downloaded data, as for this exercise we already have the data directory i set the pathway in the variable 
 mkdir -p "$destination_directory"
