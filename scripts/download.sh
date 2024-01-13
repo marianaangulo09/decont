eed2cf9c2765729295c53bf162d6c2212f759c2a
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-13 18:59:08 +0100

    This new fragment of code adds the fucntion to calculate the MD5
    In this new fragment i have included the function to be abble to check for the MD5

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,13 +2,29 @@
 
 #Assign the variables 
 
-url_file=$1
-destination_directory=$2
+# absolute path for the provided file
+url_file="$(realpath "$1")"
+destination_directory="/home/mariana/Linux_entregable/decont/data"
 uncompress=$3
 exclude_keyword=$4
 expected_md5=$5
 
-# Create a 
+#Create a  the directory for the downloaded data, as for this exercise we already have the data directory i set the pathway in the variable 
+mkdir -p "$destination_directory"
+
+
+# Download the files listed in the URL file
+wget -i "$url_file" -P "$destination_directory"
+
+
+# Function to calculate MD5 checksum for a file
+calculate_md5() {
+    md5sum "$1" | awk '{print $1}'
+}
+
+
+
+
 
 
 
