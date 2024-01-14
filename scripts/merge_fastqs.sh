f3eb20e4436bf92e50936dc6f8720754865faeaa
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-14 23:21:33 +0100

    The last version of merge script

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -11,9 +11,6 @@ mkdir -p "$output_directory"
 cd "$(dirname "$samples_directory")" || exit
 
 
-# Change to the samples directory
-cd "$(dirname "$samples_directory")" || exit
-
 # Merge all files for the specified sample ID into a single file
 cat "${sample_id}"*.1.1s_sRNA.fastq.gz "${sample_id}"*.1.2s_sRNA.fastq.gz > "$output_directory"/"${sample_id}"_merged.fastq.gz
 
