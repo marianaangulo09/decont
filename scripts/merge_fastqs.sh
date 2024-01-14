1096bd0c9a0e48064f078196b733383a92914bc6
Mariana Angulo <marianaangulo09@gmail.com>
2024-01-14 11:18:42 +0100

    This is an uodate of the merge script correcting some erros
    IN this version the script for merging is now working and the problem of not finding the files have been solved

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -10,6 +10,10 @@ mkdir -p "$output_directory"
 # Change to the samples directory
 cd "$(dirname "$samples_directory")" || exit
 
+
+# Change to the samples directory
+cd "$(dirname "$samples_directory")" || exit
+
 # Merge all files for the specified sample ID into a single file
 cat "${sample_id}"*.1.1s_sRNA.fastq.gz "${sample_id}"*.1.2s_sRNA.fastq.gz > "$output_directory"/"${sample_id}"_merged.fastq.gz
 
